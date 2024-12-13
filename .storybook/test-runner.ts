396f825bf747970a256f3832bca7e515524a618a
middle-ages-short <ran@shortstorybox.com>
2024-12-13 09:23:59 -0800

    2

---
+++ .storybook/test-runner.ts
@@ -0,0 +1,19 @@
+import type { TestRunnerConfig } from "@storybook/test-runner";
+import { argosScreenshot } from "@argos-ci/storybook";
+
+const config: TestRunnerConfig = {
+  async postVisit(page, context) {
+    await argosScreenshot(page, context);
+  },
+};
+
+export default config;import type { TestRunnerConfig } from "@storybook/test-runner";
+import { argosScreenshot } from "@argos-ci/storybook";
+
+const config: TestRunnerConfig = {
+  async postVisit(page, context) {
+    await argosScreenshot(page, context);
+  },
+};
+
+export default config;
